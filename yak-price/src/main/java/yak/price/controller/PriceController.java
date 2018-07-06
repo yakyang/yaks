a0ab4331d6b21c3e64174f73ef26fe67f631e729
yakyang@163.com <yakyang@163.com@10.6.211.94>
2018-07-06 13:08:01 +0800

    22

--- yak-price/src/main/java/yak/price/controller/PriceController.java
+++ yak-price/src/main/java/yak/price/controller/PriceController.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +16,8 @@ public class PriceController {
     /**
      * 获取商品列表
      * @return
-     */
-    @GetMapping(value = "getPriceByProductCode/{productCode}")
-    @ResponseBody
+     */   
+    @RequestMapping(value = "/getPriceByProductCode/{productCode}", method = RequestMethod.GET)
     public BigDecimal getProductPriceInfo(@PathVariable("productCode") String productCode) {
 
         return  BigDecimal.valueOf(20.00);
--- yak-product/src/main/java/yak/product/YakProductApplication.java
+++ yak-product/src/main/java/yak/product/YakProductApplication.java
@@ -3,45 +3,19 @@ package yak.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.web.servlet.ErrorPage;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.client.RestTemplate;
-
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
+@EnableEurekaClient
+@EnableFeignClients
 @EntityScan({"yak.product"})
 public class YakProductApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YakProductApplication.class, args);
-	}
-
-	@Bean
-	@LoadBalanced
-	public RestTemplate restTemplate(){
-		return new RestTemplate();
-	}
-
-	@Bean
-	public EmbeddedServletContainerCustomizer containerCustomizer() {
-
-		return new EmbeddedServletContainerCustomizer() {
-			@Override
-			public void customize(ConfigurableEmbeddedServletContainer container) {
-
-				ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/403.html");
-				ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
-				ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
-
-				container.addErrorPages(error401Page, error404Page, error500Page);
-			}
-		};
 	}
 	
 }
